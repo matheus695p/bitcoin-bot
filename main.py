5837a27632d0db6b55efe387f63417049f1bc5b3
matheus695p <mateu_695p@hotmail.com>
2021-02-20 14:56:14 -0300

    add: agregar mas decimales

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ def main():
     freq_consulta = 60
     min_iteraciones = 10
     while True:
+        print("iteracion número: ", contador)
         ahora = datetime.now().replace(microsecond=0)
         consulta = get_bt_price_pesos()
         historial.append([ahora, consulta])
+        texto =\
+            f"precio bitcoin: {round(consulta / 10**6, 9)} Millones de pesos"
         if contador <= min_iteraciones:
             pass
-            texto = f"precio bitcoin: {round(consulta / 10**6, 3)} Millones de pesos"
             send_message_telegram(texto, ahora)
         else:
             fecha, valor = get_close_price()
@@ -31,7 +33,6 @@ def main():
             # implementar inteligencia sobre logicas de vender o comprar
             analisis["valor_cierre"] = valor * 750
 
-            texto = f"precio bitcoin: {round(consulta / 10**6, 3)} Millones de pesos"
             send_message_telegram(texto, ahora)
         contador += 1
         # tiempo entre consultas
